0cb772497fc372623abf9df3679bdf42ee661607
Morgan Joyce <84004324+mojo-py@users.noreply.github.com>
2021-10-17 16:07:04 -0500

    Update fedDebt.py
    easier to digest result

--- fedDebt.py
+++ fedDebt.py
@@ -14,13 +14,16 @@ url = endpoint + f'?fields={fields}' + f'&filter={filters}'
 response = requests.get(url)
 j = json.loads(response.text)
 df = pd.DataFrame(j['data'])
+df.set_index('record_date', inplace=True)
+
+
+#Population estimates, July 1, 2019, (V2019) [US Census Bureau]
+population = 328_239_523
 
 df['netChange'] = df['tot_pub_debt_out_amt'].astype(float) - df.shift(1)['tot_pub_debt_out_amt'].astype(float)
+perCapitaDebtChangeOctober14 = df.loc['2021-10-14']['netChange']
 
-df.fillna(0, inplace = True)
+result = perCapitaDebtChangeOctober14 / population
+debtChange = '${:.2f}'.format(result)
 
-print(
-    df['netChange'].apply(lambda x: '${:.3f}B'.format((x/1_000_000_000))),
-    '\n')
-
-ax = df.tail(30).plot.bar(x='record_date', y='netChange')
+print(f'On October 14, 2021 the nation\'s debt ceiling was raised and every man, woman and child suddenly owed an additional {debtChange} in federal debt')
